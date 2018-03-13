ccffc3f1cbbdd5f64f09151ce873d4c314863f85
donhk <hk.fred.0@gmail.com>
2018-03-12 21:10:41 -0600

    added dirs

--- webx/webx.js
+++ webx/webx.js
@@ -1,10 +1,12 @@
 //ES6 arrow function
 //dependencies
+//https://stackoverflow.com/questions/5178334/folder-structure-for-a-node-js-project
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 
-const app = express();
-
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -13,6 +15,6 @@ app.listen(process.env.PORT || 6015, () => {
 });
 
 app.get('/', (req, res) => {
-   console.log('loading page');
-   res.json('done');    
+    console.log('loading page');
+    res.render('index.ejs')
 });
