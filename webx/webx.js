984dae522243ddbff48d5eabad57b4f6300b9eac
donhk <hk.fred.0@gmail.com>
2018-03-13 22:48:23 -0600

    testing front end

--- webx/webx.js
+++ webx/webx.js
@@ -4,17 +4,38 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const port = 6015;
+
+const connection = mysql.createConnection({
+    host: '192.168.0.16',
+    user: 'dbboy',
+    password: 'Welcome123+',
+    database: 'rockola'
+});
+
+connection.connect();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(process.env.PORT || 6015, () => {
-    console.log('listening on 6015')
+app.listen(process.env.PORT || port, () => {
+    console.log('listening on ' + port)
 });
 
 app.get('/', (req, res) => {
     console.log('loading page');
     res.render('index.ejs')
 });
+
+app.get('/tracks', (req, res) => {
+    console.log('loading page');
+    connection.query("select title, hash, length, album, year, trackNo, bitrate, size from Tracks", function (err, result, fields) {
+        if (err) throw err;
+        console.log("serving list of tracks");
+        res.json(result);
+    });
+});
+
